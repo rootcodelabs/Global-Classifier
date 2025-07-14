256c19ad52eb524bf3c2286e3679408ff584c4df
erangi-ar <erangika.ariyasena@rootcode.io>
2025-07-14 10:26:13 +0530

    feat: Implement data model deletion functionality with confirmation modals and UI updates

--- GUI/src/pages/DataModels/ConfigureDataModel.tsx
+++ GUI/src/pages/DataModels/ConfigureDataModel.tsx
@@ -7,14 +7,14 @@ import BackArrowButton from 'assets/BackArrowButton';
 
 import DataModelForm from 'components/molecules/DataModelForm';
 import { getChangedAttributes } from 'utils/dataModelsUtils';
-import { Platform, UpdateType } from 'enums/dataModelsEnums';
+import { Maturity, UpdateType } from 'enums/dataModelsEnums';
 import { ButtonAppearanceTypes } from 'enums/commonEnums';
 import CircularSpinner from 'components/molecules/CircularSpinner/CircularSpinner';
 import { DataModel, UpdatedDataModelPayload } from 'types/dataModels';
 import { dataModelsQueryKeys } from 'utils/queryKeys';
 import { useTranslation } from 'react-i18next';
 import './DataModels.scss';
-import { configureDataModel, getDataModelMetadata } from 'services/datamodels';
+import { configureDataModel, deleteDataModel, getDataModelMetadata, getProductionDataModel } from 'services/datamodels';
 import { use } from 'i18next';
 import { set } from 'date-fns';
 import { areArraysEqual } from 'utils/commonUtilts';
@@ -23,8 +23,6 @@ const ConfigureDataModel: FC = () => {
   const { t } = useTranslation();
   const { open, close } = useDialog();
   const navigate = useNavigate();
-  const [enabled, setEnabled] = useState<boolean>(true);
-
   const [modalOpen, setModalOpen] = useState(false);
   const [modalType, setModalType] = useState('');
   const [modalTitle, setModalTitle] = useState<string>('');
@@ -32,9 +30,15 @@ const ConfigureDataModel: FC = () => {
   const modalFunciton = useRef(() => { });
   const [searchParams] = useSearchParams();
   const modelId = searchParams.get('datamodelId');
+
   const { data: modelMetadata } = useQuery({
     queryKey: dataModelsQueryKeys.GET_META_DATA(modelId ?? ''),
     queryFn: () => getDataModelMetadata(modelId ?? ''),
+  });
+
+  const { data: prodDataModel, isLoading: isProdDataModelLoading } = useQuery({
+    queryKey: dataModelsQueryKeys.GET_PROD_DATA_MODEL(),
+    queryFn: () => getProductionDataModel(),
   });
 
   const [initialData, setInitialData] = useState<Partial<DataModel>>({
@@ -122,11 +126,58 @@ const ConfigureDataModel: FC = () => {
       updateType: updateType ?? "",
     };
 
-    updateMutation.mutate(updatedPayload);
+    if (updateType) {
+      if (prodDataModel && dataModel.deploymentEnvironment === "production") {
+        openModal(
+          t('dataModels.createDataModel.replaceDesc'),
+          t('dataModels.createDataModel.replaceTitle'),
+          () => updateMutation.mutate(updatedPayload),
+          'replace'
+        );
+      } else {
+        updateMutation.mutate(updatedPayload);
+      }
+    }
 
   };
 
+  const deleteDataModelMutation = useMutation({
+    mutationFn: deleteDataModel,
+    onSuccess: () => {
+      open({
+        title: t('dataModels.configureDataModel.deleteModalSuccessTitle'),
+        content: t('dataModels.configureDataModel.deleteModalSuccessDesc'),
+        footer: (
+          <div className='flex-grid'>
+            <Button onClick={() => { navigate('/data-models'); close(); }}>View all Data Models</Button>
+          </div>
+        ),
+      });
+    },
+    onError: () => {
+      open({
+        title: t('dataModels.configureDataModel.deleteModalErrorTitle'),
+        content: t('dataModels.configureDataModel.deleteModalErrorDesc'),
+      });
+    },
+  });
+
   const handleDelete = () => {
+    if (dataModel.deploymentEnvironment === Maturity.PRODUCTION) {
+      openModal(
+        t('dataModels.configureDataModel.deleteErrorDesc'),
+        t('dataModels.configureDataModel.deleteErrorTitle'),
+        () => navigate('/data-models'),
+        'warning'
+      );
+    } else {
+      openModal(
+        t('dataModels.configureDataModel.deleteConfirmationDesc'),
+        t('dataModels.configureDataModel.deleteConfirmation'),
+        () => deleteDataModelMutation.mutate(modelId),
+        'delete'
+      );
+    }
 
   };
 
@@ -143,7 +194,7 @@ const ConfigureDataModel: FC = () => {
     setModalTitle(title);
     modalFunciton.current = onConfirm;
   };
-  
+
   return (
     <div>
       <div className="container">
@@ -156,7 +207,7 @@ const ConfigureDataModel: FC = () => {
           </div>
         </div>
 
-        {/* <Card>
+        {modelMetadata?.modelStatus === "deprecated" && (
           <div
             className='metadata-card'
           >
@@ -165,13 +216,13 @@ const ConfigureDataModel: FC = () => {
               <Button
                 onClick={() => {
                 }}
+                appearance={ButtonAppearanceTypes.ERROR}
               >
                 {t('dataModels.configureDataModel.retrain')}
               </Button>
             </div>
           </div>
-        </Card> */}
-
+        )}
         {false ? (
           <CircularSpinner />
         ) : (
@@ -189,15 +240,15 @@ const ConfigureDataModel: FC = () => {
       >
         <Button
           appearance="error"
-          // disabled={deleteDataModelMutation.isLoading}
-          // showLoadingIcon={deleteDataModelMutation.isLoading}
+          disabled={deleteDataModelMutation.isLoading}
+          showLoadingIcon={deleteDataModelMutation.isLoading}
           onClick={() => handleDelete()}
         >
           {t('dataModels.configureDataModel.deleteModal')}
         </Button>
         <Button
           disabled={!dataModel.datasetId || dataModel.datasetId === 0}
-          // onClick={handleSaveChanges}
+          onClick={() => { }}
         // showLoadingIcon={updateMutation.isLoading}
 
         >
@@ -234,22 +285,25 @@ const ConfigureDataModel: FC = () => {
               </Button>
             ) : modalType === 'delete' ? (
               <Button
-                // disabled={deleteDataModelMutation.isLoading}
-                // showLoadingIcon={deleteDataModelMutation.isLoading}
+                disabled={deleteDataModelMutation.isLoading}
+                showLoadingIcon={deleteDataModelMutation.isLoading}
                 onClick={() => modalFunciton.current()}
                 appearance={ButtonAppearanceTypes.ERROR}
               >
                 {t('global.delete')}
               </Button>
-            ) : (
-              <Button
-                // disabled={updateDataModelMutation.isLoading}
-                // showLoadingIcon={updateDataModelMutation.isLoading}
-                onClick={() => modalFunciton.current()}
-              >
-                {t('global.proceed')}
-              </Button>
-            )}
+            )
+              : modalType === 'warning' ? (
+                <Button
+                  onClick={() => modalFunciton.current()}
+                  appearance={ButtonAppearanceTypes.PRIMARY}
+                >
+                  View all Data Models
+                </Button>
+              )
+                : (
+                  null
+                )}
           </div>
         }
       >
