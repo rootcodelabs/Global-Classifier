2fa95dfb295295efacf601165be1da5fe5c2de5a
nuwangeek <charith.bimsara@rootcode.io>
2025-07-16 15:46:30 +0530

    changed model s3 path

--- src/training/scripts/s3_utility_handler.py
+++ src/training/scripts/s3_utility_handler.py
@@ -137,14 +137,14 @@ class S3DatasetService:
             # Create zip file name with timestamp
             timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
             model_id = str(model_id)
-            zip_filename = f"{model_type}_dataset_{model_id}_model_{timestamp}.zip"
+            zip_filename = f"{model_id}.zip"
             zip_path = os.path.join(self.models_dir, zip_filename)
 
             # Zip the model directory
             self._zip_directory(model_dir, zip_path)
 
             # Upload to S3
-            s3_dest_path = f"models/{model_id}/{zip_filename}"
+            s3_dest_path = f"models/undeployed/{zip_filename}"
 
             logger.info(f"Uploading zipped model to S3: {s3_dest_path}")
 
