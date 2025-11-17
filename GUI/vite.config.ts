527cad0d933b84cccde91f93b175153d3091474f
erangi-ar <erangika.ariyasena@rootcode.io>
2025-11-17 18:47:53 +0530

    fix: Update domain references from global-classifier-dev.rootcode.software to dev-gloclf.buerokratt.ee in configuration files

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
         'Content-Security-Policy': process.env.REACT_APP_CSP,
       }),
     },
-    allowedHosts: ['global-classifier-dev.rootcode.software', 'est-global-classifier-rtc.rootcode.software', 'localhost', '127.0.0.1'],
+    allowedHosts: ['dev-gloclf.buerokratt.ee', 'est-global-classifier-rtc.rootcode.software', 'localhost', '127.0.0.1'],
   },
   resolve: {
     alias: {
